4a39905b34e99b5b6cb6bba07f8ddac01f4737d9
Hendry Lim <myhendry@gmail.com>
2020-04-10 12:12:00 +0800

    added lottie

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,28 @@
-import React from "react"
+import React, { useEffect, createRef } from "react"
 import { Link } from "gatsby"
 import { connect } from "react-redux"
+import lottie from "lottie-web"
+import styled from "styled-components"
 
+import animation from "../animations/17895-wear-mask.json"
 import { Container } from "../components/common"
 import SEO from "../components/seo"
 import { toggleDarkMode } from "../redux/actions"
 
 const IndexPage = props => {
+  let animationContainer = createRef()
+
+  useEffect(() => {
+    const anim = lottie.loadAnimation({
+      container: animationContainer.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: animation,
+    })
+    return () => anim.destroy() // optional clean up for unmounting
+  }, [])
+
   return (
     <Container>
       <SEO title="Home" />
@@ -19,13 +35,21 @@ const IndexPage = props => {
         >
           Using Redux
         </button>
+        <div>
+          <Link to="/account">Go to your account</Link>
+        </div>
       </div>
       <div>
-        <Link to="/account">Go to your account</Link>
+        <AnimationContainer ref={animationContainer} />
       </div>
     </Container>
   )
 }
+
+const AnimationContainer = styled.div`
+  width: 30rem;
+  height: 30rem;
+`
 
 export default connect(state => {
   return {
